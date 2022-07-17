8d59a15f04087b11ae7d1249641ec4bd5ab1d5dd
Michał Smolarek <msmolarek96@gmail.com>
2022-07-17 17:28:00 +0200

    put the bloc consumer into ui

--- lib/pages/weather_search_page.dart
+++ lib/pages/weather_search_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_cubit_bloc_tutorial/cubit/weather_cubit.dart';
 import 'package:flutter_cubit_bloc_tutorial/data/model/weather.dart';
 
 class WeatherSearchPage extends StatefulWidget {
@@ -14,11 +16,27 @@ class _WeatherSearchPageState extends State<WeatherSearchPage> {
         title: Text("Weather Search"),
       ),
       body: Container(
-        padding: EdgeInsets.symmetric(vertical: 16),
-        alignment: Alignment.center,
-        // TODO: Implement with cubit
-        child: buildInitialInput(),
-      ),
+          padding: EdgeInsets.symmetric(vertical: 16),
+          alignment: Alignment.center,
+          child: BlocConsumer<WeatherCubit, WeatherState>(
+            listener: (context, state) {
+              if (state is WeatherError) {
+                ScaffoldMessenger.of(context)
+                    .showSnackBar(SnackBar(content: Text(state.message)));
+              }
+            },
+            builder: (context, state) {
+              if (state is WeatherInitial) {
+                return buildInitialInput();
+              } else if (state is WeatherLoading) {
+                return buildLoading();
+              } else if (state is WeatherLoaded) {
+                return buildColumnWithData(state.weather);
+              } else {
+                return buildInitialInput();
+              }
+            },
+          )),
     );
   }
 
@@ -74,6 +92,7 @@ class CityInputField extends StatelessWidget {
   }
 
   void submitCityName(BuildContext context, String cityName) {
-    // TODO: Get weather for the city
+    final weatherCubit = context.read<WeatherCubit>();
+    weatherCubit.getWeather(cityName);
   }
 }
